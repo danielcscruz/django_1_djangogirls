b511bb834e87578a0c3be06307191c3fa216e112
Daniel <danielcscruz@gmail.com>
2020-06-22 16:55:35 -0300

    adiciona templates

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blogapp/post_list.html', {})
+    posts = Post.objects.filter().order_by('published_date')
+    return render(request, 'blogapp/post_list.html', {'posts': posts})
